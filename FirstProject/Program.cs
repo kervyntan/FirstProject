9503204cf5d16f72258356ddb75181be327a3954
Kervyn <kervyntan@gmail.com>
2023-05-15 12:10:51 +0800

    Update first lines

---
+++ FirstProject/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FirstProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Type Casting
+        double a = 3.14;
+        // Int -> decimals are truncated
+        int b = Convert.ToInt32(a);
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine("Hello, World!");
+    }
+}
+
